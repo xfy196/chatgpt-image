2c139e95f6b1c44d95ea947403d07222fd069bba
xufuying <xufuying@100doc.com.cn>
2023-04-13 10:22:24 +0800

    更改openai的key

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,3 @@
 export const openapiSk =
-  "c2stNzBMVktzQjlPaEhiUzJwSEJnNkpUM0JsYmtGSnZ0V1FTSlZsZmh0VE85anZQbEFZ";
+  "c2stSjJIY2JPWjYxeTNnMTk4aUZaZHBUM0JsYmtGSldDbXJnQjRFaG54M3hyMEdQNjBN";
 export const originId = "org-XsZGOal8prIyT7c84DIxOm6V";
